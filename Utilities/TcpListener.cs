1270d2f338726ce1e691ded3fb960e96163204ad
Rizzzi Git <rizzzigit@gmail.com>
2024-10-23 23:24:48 +0800

    add `TcpListener` extensions

---
+++ Utilities/TcpListener.cs
@@ -0,0 +1,18 @@
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace RizzziGit.Commons.Utilities;
+
+public static class TcpListenerExceptions
+{
+    public static async IAsyncEnumerable<TcpClient> ToAsyncEnumerable(
+        this TcpListener listener,
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        while (true)
+        {
+            yield return await listener.AcceptTcpClientAsync(cancellationToken);
+        }
+    }
+}
