efe61d417b171eccf36380b2b3accee211839449
Rizzzi Git <rizzzigit@gmail.com>
2024-11-09 21:20:56 +0800

    add custom message parameter

--- Utilities/Exception.cs
+++ Utilities/Exception.cs
@@ -2,6 +2,6 @@ namespace RizzziGit.Commons.Utilities;
 
 public static class ExceptionExtensions
 {
-    public static string ToPrintable(this Exception exception) =>
-        $"{exception.Message}{(exception.StackTrace != null ? $"\n{exception.StackTrace}" : "")}";
+    public static string ToPrintable(this Exception exception, string? customMessage = null) =>
+        $"{customMessage ?? exception.Message}{(exception.StackTrace != null ? $"\n{exception.StackTrace}" : "")}";
 }
