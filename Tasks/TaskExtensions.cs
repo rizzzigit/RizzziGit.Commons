028b658fa99fc8848d0d4a6da8542e7ac4840bae
Rizzzi Git <rizzzigit@gmail.com>
2024-12-24 15:08:29 +0000

    add more task extension methods

--- Tasks/TaskExtensions.cs
+++ Tasks/TaskExtensions.cs
@@ -12,4 +12,28 @@ public static class TaskExtensions
 
     public static async Task Then<V>(this Task<V> task, Func<V, Task> callback) =>
         await callback(await task);
+
+    public static async Task<T> Then<T>(this Task task, Func<Task<T>> callback)
+    {
+        await task;
+        return await callback();
+    }
+
+    public static async Task<T> Then<T>(this Task task, Func<T> callback)
+    {
+        await task;
+        return callback();
+    }
+
+    public static async Task Then(this Task task, Func<Task> callback)
+    {
+        await task;
+        await callback();
+    }
+
+    public static async Task Then(this Task task, Action callback)
+    {
+        await task;
+        callback();
+    }
 }
